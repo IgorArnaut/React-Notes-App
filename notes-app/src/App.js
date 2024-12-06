f70967530c5eac2e5fec77a47c25ad07decdd018
Igor <igor.arnautovic123@outlook.com>
2024-12-06 08:43:37 +0100

    Added list of notes

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,3 +1,24 @@
+function Note({ note }) {
+  return (
+    <div>
+      <h1>{note.title}</h1>
+      <h2>{note.date}</h2>
+      <p>{note.text}</p>
+    </div>
+  );
+}
+
 export default function App() {
-  return <p>Hello World</p>;
+  const noteList = [
+    { title: "Lorem ipsum", date: "2024-12-06", text: "Lorem ipsum dolor sit amet" },
+    { title: "Consectetur", date: "2024-12-06", text: "Consectetur adipiscing elit" },
+    { title: "Sed do eiusmod", date: "2024-12-06", text: "Sed do eiusmod tempor incididunt" },
+  ];
+  return (
+    <>
+      {noteList.map((note) => (
+        <Note note={note} />
+      ))}
+    </>
+  );
 }
