34498059f00195955cf99e78560f8327f6ea3b6e
Igor <igor.arnautovic123@outlook.com>
2024-12-06 12:05:12 +0100

    Refactored code

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -8,17 +8,20 @@ export default function App() {
   const noteList = [];
   const [notes, setNotes] = useState(noteList);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (e.target.title.value === "") return;
-    if (e.target.text.value === "") return;
+  const createNewNote = (e) => {
     const newNote = {
       title: e.target.title.value,
       date: moment().format("MM/DD/yyyy hh:mm"),
       text: e.target.text.value,
     };
-    console.log(newNote);
     setNotes([...notes, newNote]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (e.target.title.value === "") return;
+    if (e.target.text.value === "") return;
+    createNewNote(e);
   };
 
   return (
--- notes-app/src/NoteForm.js
+++ notes-app/src/NoteForm.js
@@ -2,9 +2,11 @@ export default function NoteForm({ handleSubmit }) {
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title">Title</label>
+      <br />
       <input type="text" id="title" />
       <br />
-      <label htmlFor="text">Title</label>
+      <label htmlFor="text">Text</label>
+      <br />
       <textarea id="text"></textarea>
       <br />
       <input type="submit" value="Save" />
